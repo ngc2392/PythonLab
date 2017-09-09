5765a948dd9c4c5f63fd28441bd1f68c58d333d5
Logan Phillips <LoganPhillips@logans-mbp.lvc.edu>
2017-09-09 01:52:35 -0400

    Created assignment 3 folder with three python files.  Each of these files will be a sub assignment in assignment 3

---
+++ Assignment3/ContainsSubstring.py
@@ -0,0 +1,3 @@
+#Write a pythin program that asks the user for five text strings.  The program
+#should then report on whehter any of those strings is a sub-string of another (all such combinations
+#should be reported)
