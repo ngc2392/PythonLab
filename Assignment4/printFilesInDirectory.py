98b81afdf4d3bf4aaee4bad4e866d8f963b7631c
Logan Phillips <LoganPhillips@logans-mbp.lvc.edu>
2017-09-09 18:14:33 -0400

    part 2 of assignment 2 - printFilesInDirectory.py.  No dialog box

--- Assignment4/printFilesInDirectory.py
+++ Assignment4/printFilesInDirectory.py
@@ -3,7 +3,26 @@ import os
 #print all files in directory
 #relative and absolute?
 
-os.listdir("/desktop")
+#print(os.listdir("/Users/LoganPhillips/Desktop"))
 
+#/Users/LoganPhillips
 
+#https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
+def files_in_directory(path, extension):
+    print(extension)
+    arr_files = [x for x in os.listdir(path) if x.endswith("."+extension)]
+    return arr_files
 
+def everything_in_directory(path):
+    arr_files = [x for x in os.listdir(path)]
+    return arr_files
+
+files = files_in_directory("/Users/LoganPhillips/Desktop", "pdf")
+
+for x in range(len(files)):
+    print(files[x])
+
+files = everything_in_directory("/Users/LoganPhillips/Desktop")
+
+for x in range(len(files)):
+    print(files[x])
